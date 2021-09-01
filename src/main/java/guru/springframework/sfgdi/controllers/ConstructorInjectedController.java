24da913592899d87c7e12ec78b2d6e1758cbee3d
KhalloufiHamza <hamza.khalloufi@usmba.ac.ma>
2021-09-01 18:28:50 +0100

    final

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,4 +1,14 @@
 package guru.springframework.sfgdi.controllers;
 
+import guru.springframework.sfgdi.services.GreetingService;
+
 public class ConstructorInjectedController {
+    public GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
 }
